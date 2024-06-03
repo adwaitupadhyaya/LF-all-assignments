582941a49297d0b4e5cdac5252f3308e384276be
Adwait Upadhyaya <adwait627@gmail.com>
2024-06-04 02:24:34 +0545

    toggle text color fix

--- Assignment 2/scripts.js
+++ Assignment 2/scripts.js
@@ -16,23 +16,27 @@ lightModeButton.addEventListener("click", () => {
 const enableLightMode = () => {
   document.body.classList.add("light-mode");
 
-  // Select and remove specific div elements
   const divToRemove1 = document.getElementById("purple");
   if (divToRemove1) {
-    removedDivs.push(divToRemove1); // Store the removed div
+    removedDivs.push(divToRemove1);
     divToRemove1.remove();
   }
   const divToRemove2 = document.getElementById("blue");
   if (divToRemove2) {
-    removedDivs.push(divToRemove2); // Store the removed div
+    removedDivs.push(divToRemove2);
     divToRemove2.remove();
   }
+
+  const divsWithTextColor = document.querySelectorAll(".change-text-color");
+  divsWithTextColor.forEach((div) => div.classList.add("dark-text-color"));
 };
 
 const disableLightMode = () => {
   document.body.classList.remove("light-mode");
 
-  // Re-add the stored divs back to the DOM
   removedDivs.forEach((div) => document.body.appendChild(div));
-  removedDivs = []; // Clear the array after re-adding the divs
+  removedDivs = [];
+
+  const divsWithTextColor = document.querySelectorAll(".change-text-color");
+  divsWithTextColor.forEach((div) => div.classList.remove("dark-text-color"));
 };
