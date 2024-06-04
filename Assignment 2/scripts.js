6f0b087ace2ef81ea342f001f355b4111f9b0c83
Adwait Upadhyaya <adwait627@gmail.com>
2024-06-04 09:43:19 +0545

    final changes

--- Assignment 2/scripts.js
+++ Assignment 2/scripts.js
@@ -16,17 +16,6 @@ lightModeButton.addEventListener("click", () => {
 const enableLightMode = () => {
   document.body.classList.add("light-mode");
 
-  const divToRemove1 = document.getElementById("purple");
-  if (divToRemove1) {
-    removedDivs.push(divToRemove1);
-    divToRemove1.remove();
-  }
-  const divToRemove2 = document.getElementById("blue");
-  if (divToRemove2) {
-    removedDivs.push(divToRemove2);
-    divToRemove2.remove();
-  }
-
   const divsWithTextColor = document.querySelectorAll(".change-text-color");
   divsWithTextColor.forEach((div) => div.classList.add("dark-text-color"));
   const divsWithBgColor = document.querySelectorAll(".change-bg-color");
@@ -35,9 +24,6 @@ const enableLightMode = () => {
 
 const disableLightMode = () => {
   document.body.classList.remove("light-mode");
-
-  removedDivs.forEach((div) => document.body.appendChild(div));
-  removedDivs = [];
 
   const divsWithTextColor = document.querySelectorAll(".change-text-color");
   divsWithTextColor.forEach((div) => div.classList.remove("dark-text-color"));
