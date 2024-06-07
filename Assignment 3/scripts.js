7eb8a101a85431f05ca2ef375d9e3fe3abe0cdac
Adwait Upadhyaya <adwait627@gmail.com>
2024-06-07 22:06:24 +0545

    random direction and  colors, variable ball count,speed and size

--- Assignment 3/scripts.js
+++ Assignment 3/scripts.js
@@ -1,9 +1,8 @@
 class Ball {
-  constructor(x, y, w, h, color, dy, dx) {
+  constructor(x, y, radius, color, dy, dx) {
     this.x = x;
     this.y = y;
-    this.w = w;
-    this.h = h;
+    this.radius = radius;
     this.color = color;
     this.borderRadius = "50%";
     this.dy = dy;
@@ -12,45 +11,74 @@ class Ball {
     this.newBall = document.createElement("div");
     // create styling to ball
     this.drawBall = () => {
-      this.newBall.style.backgroundColor = ballObject.color;
-      this.newBall.style.width = `${ballObject.w}px`;
-      this.newBall.style.height = `${ballObject.h}px`;
-      this.newBall.style.top = `${ballObject.y}px`;
-      this.newBall.style.left = `${ballObject.x}px`;
+      this.newBall.style.backgroundColor = this.color;
+      this.newBall.style.width = `${this.radius}px`;
+      this.newBall.style.height = `${this.radius}px`;
+      this.newBall.style.top = `${this.y}px`;
+      this.newBall.style.left = `${this.x}px`;
       this.newBall.style.position = "absolute";
-      this.newBall.style.borderRadius = ballObject.borderRadius;
+      this.newBall.style.borderRadius = this.borderRadius;
     };
   }
 }
 
-const BALL_BOX_HEIGHT = 700;
-const BALL_BOX_WIDTH = 700;
+const BALL_BOX_HEIGHT = 500;
+const BALL_BOX_WIDTH = 500;
+const NUMBER_OF_BALLS = 10;
 
+const BALL_COLORS_ARRAY = [
+  "red",
+  "green",
+  "yellow",
+  "purple",
+  "black",
+  "gray",
+  "orange",
+];
 // give styling to box
 const ballBox = document.getElementById("box");
 ballBox.style.position = "relative";
 ballBox.style.height = `${BALL_BOX_HEIGHT}px`;
 ballBox.style.width = `${BALL_BOX_WIDTH}px`;
 
-// create ball object
-const ballObject = new Ball(10, 20, 50, 50, "green", 10, 3);
+// randomize creation of ball
 
-// add ball to box
-let isGreen = true;
+for (let i = 0; i < NUMBER_OF_BALLS; i++) {
+  const randomColor = Math.round(Math.random() * 7);
+  const randomRadius = Math.round(Math.random() * (100 - 10) + 10);
+  const randomTop = Math.round(Math.random() * BALL_BOX_HEIGHT);
+  const randomLeft = Math.round(Math.random() * BALL_BOX_WIDTH - 100);
+  const randomDx = Math.round(Math.random() * 10);
+  const randomDy = Math.round(Math.random() * 10);
 
-setInterval(() => {
-  ballObject.y += ballObject.dy;
-  ballObject.x += ballObject.dx;
-  if (ballObject.y + ballObject.h > BALL_BOX_HEIGHT || ballObject.y < 0) {
-    ballObject.dy = -1 * ballObject.dy;
-  }
+  const ballObject = new Ball(
+    randomTop,
+    randomLeft,
+    randomRadius,
+    BALL_COLORS_ARRAY[randomColor],
+    randomDx,
+    randomDy
+  );
 
-  if (ballObject.x + ballObject.w > BALL_BOX_WIDTH || ballObject.x < 0) {
-    ballObject.dx = -1 * ballObject.dx;
-  }
-
-  console.log("here");
+  // add balls to box
+  ballBox.appendChild(ballObject.newBall);
+  // draw the initial state of the balls
   ballObject.drawBall();
-}, 20);
+  setInterval(() => {
+    moveBall(ballObject);
+  }, 20);
+}
 
-ballBox.appendChild(ballObject.newBall);
+const moveBall = (ballToMove) => {
+  ballToMove.y += ballToMove.dy;
+  ballToMove.x += ballToMove.dx;
+  if (ballToMove.y + ballToMove.radius > BALL_BOX_HEIGHT || ballToMove.y < 0) {
+    ballToMove.dy = -1 * ballToMove.dy;
+  }
+
+  if (ballToMove.x + ballToMove.radius > BALL_BOX_WIDTH || ballToMove.x < 0) {
+    ballToMove.dx = -1 * ballToMove.dx;
+  }
+
+  ballToMove.drawBall();
+};
