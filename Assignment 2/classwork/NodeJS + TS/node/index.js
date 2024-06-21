87bcf7fba0b9583eff7c79654c68b91255162ba0
Adwait Upadhyaya <adwait627@gmail.com>
2024-06-21 11:30:39 +0545

    lever tweaks

--- Assignment 2/classwork/NodeJS + TS/node/index.js
+++
@@ -1,7 +0,0 @@
-import { getColor, getRandom } from "./util.js";
-
-import lodash from "lodash";
-
-console.log(lodash.capitalize("sjhfvskfd"));
-
-console.log(getColor(), getRandom());
--- Assignment 2/classwork/NodeJS + TS/ts/index.ts
+++
@@ -1,2 +0,0 @@
-let myName: string = "John";
-let num = 1;
